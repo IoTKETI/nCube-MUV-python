d8520b8dfa3720645d6ae9a334c6b8a8483fe37f
IoTKETI <iot.keti@gmail.com>
2021-04-13 19:05:59 +0900

    - edit code

--- http_app.py
+++ http_app.py
@@ -303,7 +303,7 @@ def delete_sub_all(count):
     if len(conf.conf['sub']) == 0:
         return 2001, count
     else:
-        if conf.conf['sub'].get(count):
+        if conf.conf['sub'].index(count):
             target = conf.conf['sub'][count]['parent'] + '/' + conf.conf['sub'][count]['name']
             rsc, res_body, count = http_adn.delsub(target, count)
             if rsc == 5106 or rsc == 2002 or rsc == 2000 or rsc == 4105 or rsc == 4004:
